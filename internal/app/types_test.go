2e5eee6c628b1e2ad9a7d81706a08dd4c7b9112e
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2022-06-05 13:39:36 +1000

    Types tests

---
+++ internal/app/types_test.go
@@ -0,0 +1,49 @@
+package app
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	tests := []struct {
+		name string
+		give SecretChangeSet
+		want string
+	}{
+		{
+			name: "add",
+			give: SecretChangeSet{
+				add:    []string{"apple", "pear", "orange"},
+			},
+			want: "+apple\n+pear\n+orange\n",
+		}, {
+			name: "remove",
+			give: SecretChangeSet{
+				remove: []string{"peach", "coconut"},
+			},
+			want: "-peach\n-coconut\n",
+		}, {
+			name: "add_remove",
+			give: SecretChangeSet{
+				add:    []string{"apple", "pear", "orange"},
+				remove: []string{"peach", "coconut"},
+			},
+			want: "+apple\n+pear\n+orange\n-peach\n-coconut\n",
+		}, {
+			name: "empty",
+			give: SecretChangeSet{},
+			want: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := fmt.Sprint(tt.give)
+			if got != tt.want {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
