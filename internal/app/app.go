8dea7a7c5f169c74fbfa7743d6b43ff7388a02a3
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2022-06-05 20:43:02 +1000

    Reduced Run function size

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/kafka"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/theckman/yacspin"
 
 	secretsmanagertypes "github.com/aws/aws-sdk-go-v2/service/secretsmanager/types"
 
@@ -27,22 +28,65 @@ func Run() error {
 
 	spinner.Start()
 	spinner.Message("list kafka clusters and secretsmanager secrets")
+	listClustersSecrets(svc)
 
+	spinner.Message("list scram secrets")
+	listScramSecretsByCluster(svc, spinner)
+
+	spinner.Suffix(" retrieved data")
+	spinner.Stop()
+	fmt.Println()
+
+	for _, cluster := range svc.clusters {
+		mapSecretsToClusters(cluster, svc.secrets)
+		reconcileClusterSecrets(cluster)
+	}
+
+	svc.printOverview()
+	svc.printChangeSet()
+
+	// fmt.Println("Press enter to apply changes.")
+	// fmt.Scanln()
+	//
+	// spinner.Suffix(" modifying clusters")
+	// spinner.Start()
+	// updateClustersSecrets(svc, spinner)
+	// spinner.Stop()
+
+	return nil
+}
+
+func newService() (svc *Service, err error) {
+	config, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil, fmt.Errorf("unable to create aws config: %w", err)
+	}
+
+	return &Service{
+		kafka:          kafka.NewFromConfig(config),
+		secretsmanager: secretsmanager.NewFromConfig(config),
+		clusters:       []*Cluster{},
+	}, nil
+}
+
+func listClustersSecrets(svc *Service) error {
 	g := new(errgroup.Group)
 
 	g.Go(func() error {
-		svc.clusters, err = listClusters(svc.kafka)
+		clusters, err := listClusters(svc.kafka)
 		if err != nil {
 			return fmt.Errorf("unable to list clusters: %w", err)
 		}
+		svc.clusters = clusters
 		return nil
 	})
 
 	g.Go(func() error {
-		svc.secrets, err = listSecrets(svc.secretsmanager)
+		secrets, err := listSecrets(svc.secretsmanager)
 		if err != nil {
 			return fmt.Errorf("unable to list secrets: %w", err)
 		}
+		svc.secrets = secrets
 		return nil
 	})
 
@@ -50,7 +94,14 @@ func Run() error {
 		return fmt.Errorf("unable to list clusters and secrets: %w", err)
 	}
 
+	return nil
+}
+
+func listScramSecretsByCluster(svc *Service, spinner *yacspin.Spinner) error {
+	g := new(errgroup.Group)
+
 	clusterName := make(chan string, len(svc.clusters))
+
 	g.Go(func() error {
 		format := "list scram secrets [%v/%v] - %v"
 		watchChan(clusterName, format, spinner)
@@ -72,50 +123,22 @@ func Run() error {
 		return fmt.Errorf("unable to list scram secrets: %w", err)
 	}
 
-	spinner.Suffix(" retrieved data")
-	spinner.Stop()
-	fmt.Println()
-
-	for _, cluster := range svc.clusters {
-		mapSecretsToClusters(cluster, svc.secrets)
-		reconcileClusterSecrets(cluster)
-	}
-
-	svc.printOverview()
-	svc.printChangeSet()
-
-	// fmt.Println("Press enter to apply changes.")
-	//
-	// fmt.Scanln()
-	//
-	// spinner.Suffix(" modifying clusters")
-	// spinner.Start()
-	// for _, cluster := range svc.clusters {
-	// 	name := aws.StringValue(cluster.clusterInfo.ClusterName)
-	// 	spinner.Message(fmt.Sprintf("updating scram secrets [%v]", name))
-	// 	if err := svc.associateSecrets(svc.kafka, cluster); err != nil {
-	// 		return fmt.Errorf("unable to assosciate secrets for %v: %w", name, err)
-	// 	}
-	// 	if err := svc.disassociateSecrets(svc.kafka, cluster); err != nil {
-	// 		return fmt.Errorf("unable to disassosciate secrets for %v: %w", name, err)
-	// 	}
-	// }
-	// spinner.Stop()
-
 	return nil
 }
 
-func newService() (svc *Service, err error) {
-	config, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		return nil, fmt.Errorf("unable to create aws config: %w", err)
+func updateClustersSecrets(svc *Service, spinner *yacspin.Spinner) error {
+	for _, cluster := range svc.clusters {
+		name := aws.ToString(cluster.clusterInfo.ClusterName)
+		spinner.Message(fmt.Sprintf("updating scram secrets [%v]", name))
+		if err := associateSecrets(svc.kafka, cluster); err != nil {
+			return fmt.Errorf("unable to assosciate secrets for %v: %w", name, err)
+		}
+		if err := disassociateSecrets(svc.kafka, cluster); err != nil {
+			return fmt.Errorf("unable to disassosciate secrets for %v: %w", name, err)
+		}
 	}
 
-	return &Service{
-		kafka:          kafka.NewFromConfig(config),
-		secretsmanager: secretsmanager.NewFromConfig(config),
-		clusters:       []*Cluster{},
-	}, nil
+	return nil
 }
 
 func mapSecretsToClusters(cluster *Cluster, secrets []secretsmanagertypes.SecretListEntry) error {
